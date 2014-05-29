4e0a0fbba0a2093480d801bea931548774e0c1d7
laetitiamassa <laetitiamassa@gmail.com>
2014-05-29 12:42:50 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dias::Application.config.secret_key_base = '294f40136600391d57849d165b8c36ebc3ec31991ec2d2935801f0228b207dd5aaf98b1d3a749ee18270f83e97b9fd228ae5e4b522e2aacc7ff9a542e00d70d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dias::Application.config.session_store :cookie_store, key: '_dias_session'
